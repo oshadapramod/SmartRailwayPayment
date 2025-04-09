3de063b1094799484fc177a7cc1bb551532e5682
chalanasandun <chalanasandun@gmail.com>
2025-04-09 22:15:04 +0530

    updated

---
+++ firmware/main/led.c
@@ -0,0 +1,14 @@
+#include "led.h"
+
+void led_init(void) {
+    gpio_pad_select_gpio(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+}
+
+void led_on(void) {
+    gpio_set_level(LED_PIN, 1);  // Turn on the LED
+}
+
+void led_off(void) {
+    gpio_set_level(LED_PIN, 0);  // Turn off the LED
+}
